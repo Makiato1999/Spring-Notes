773e7f9b50310d9256dd4251ea96c7db854d53dc
XIAORAN XIE <59595778+Makiato1999@users.noreply.github.com>
2024-02-16 22:59:45 -0500

    thymeleaf
    thymeleaf

--- example_19/src/main/java/com/easybytes/easyschool/controller/HomeController.java
+++ example_19/src/main/java/com/easybytes/easyschool/controller/HomeController.java
@@ -1,13 +1,15 @@
 package com.easybytes.easyschool.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
     // routing
-    @RequestMapping("/home")
-    public String displayHomePage() {
+    @RequestMapping(value={"", "/", "home"})
+    public String displayHomePage(Model model) {
+        model.addAttribute("username", "Shawn Xie");
         return "home.html";
     }
 }
