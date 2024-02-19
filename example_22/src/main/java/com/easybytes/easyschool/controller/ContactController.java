8c7e066a2f87082e272573e50761e284b2a99093
XIAORAN XIE <59595778+Makiato1999@users.noreply.github.com>
2024-02-19 17:17:25 -0500

    Query Params using @RequestParam
    Query Params using @RequestParam

---
+++ example_22/src/main/java/com/easybytes/easyschool/controller/ContactController.java
@@ -0,0 +1,51 @@
+package com.easybytes.easyschool.controller;
+
+import com.easybytes.easyschool.model.Contact;
+import com.easybytes.easyschool.service.ContactService;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
+
+@Slf4j
+@Controller
+public class ContactController {
+    // 如果使用lambok，那就不需要下面这个了
+    // private static Logger log = LoggerFactory.getLogger(ContactController.class);
+    private final ContactService contactService;
+    @Autowired
+    public ContactController(ContactService contactService) {
+        this.contactService = contactService;
+    }
+
+    @RequestMapping(value={"/contact"})
+    public String displayContactPage() {
+        //model.addAttribute("username", "Shawn Xie");
+        return "contact.html";
+    }
+
+//    @RequestMapping(value = "/saveMsg", method = POST)
+//    //@PostMapping(value = "/saveMsg")
+//    public ModelAndView saveMessage(@RequestParam String name, @RequestParam String mobileNum,
+//                                    @RequestParam String email, @RequestParam String subject,
+//                                    @RequestParam String message) {
+//        log.info("Name : "+name);
+//        log.info("mobileNum : "+mobileNum);
+//        log.info("email : "+email);
+//        log.info("subject : "+subject);
+//        log.info("message : "+message);
+//        return new ModelAndView("redirect:/contact");
+//    }
+    @RequestMapping(value = "/saveMsg", method = POST)
+    public ModelAndView saveMessage(Contact contact) {
+        contactService.saveMessageDetails(contact);
+        return new ModelAndView("redirect:/contact");
+    }
+}
