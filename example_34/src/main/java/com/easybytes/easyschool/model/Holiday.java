aef840c436a506e9cb328c935ecdaf645d17a77d
XIAORAN XIE <59595778+Makiato1999@users.noreply.github.com>
2024-03-04 16:50:17 -0500

    Migrate from H2 Database to MYSQL Database
    Migrate from H2 Database to MYSQL Database

---
+++ example_34/src/main/java/com/easybytes/easyschool/model/Holiday.java
@@ -0,0 +1,13 @@
+package com.easybytes.easyschool.model;
+
+import lombok.Data;
+
+@Data
+public class Holiday extends BaseEntity {
+    private String day;
+    private String reason;
+    private Type type;
+    public enum Type {
+        FESTIVAL, FEDERAL
+    }
+}
