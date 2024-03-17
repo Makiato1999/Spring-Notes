0721cf4de6a2841d2d8c69b983114f9314796ba1
XIAORAN XIE <59595778+Makiato1999@users.noreply.github.com>
2024-03-17 15:53:53 -0400

    Migrate from JDBCTemplate to Spring Data JPA
    Migrate from JDBCTemplate to Spring Data JPA

---
+++ example_35/src/main/java/com/easybytes/easyschool/model/BaseEntity.java
@@ -0,0 +1,15 @@
+package com.easybytes.easyschool.model;
+
+import jakarta.persistence.MappedSuperclass;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@MappedSuperclass
+public class BaseEntity {
+    private LocalDateTime createdAt;
+    private String createdBy;
+    private LocalDateTime updatedAt;
+    private String updatedBy;
+}
